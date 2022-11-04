a6161bf5b7963b8197efb344b93f94e6bd6f212d
w9577759 <w9577759@live.tees.ac.uk>
2022-11-04 14:25:27 +0000

    Login and registration page

---
+++ rED/app/src/main/java/uk/ac/tees/w9577759/red/MainActivity.java
@@ -0,0 +1,32 @@
+package uk.ac.tees.w9577759.red;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView et;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         et=findViewById(R.id.registerPage);
+
+        et.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RegistrationPage.class));
+
+            }
+        });
+
+
+    }
+}
